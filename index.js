3b9a0e5d4b5635b80942351eff1f1dcdc28f7bfb
imitrache <imitrache@luxoft.com>
2017-01-10 16:48:44 +0200

    list of vids

---
+++ index.js
@@ -0,0 +1,397 @@
+let technologies = [
+		{
+			name: 'React',
+			courses: [
+				{
+					name: 'Start Using React to Build Web Applications',
+					url: 'https://egghead.io/courses/react-fundamentals'
+				},
+				{
+					name: 'Getting Started with Redux',
+					url: 'https://egghead.io/courses/getting-started-with-redux'
+				},
+				{
+					name: 'Animate React Native UI Elements',
+					url: 'https://egghead.io/courses/animate-react-native-ui-elements'
+				},
+				 {
+				 	name: 'Manage Complex State in React Apps with MobX',
+				 	url: 'https://egghead.io/courses/manage-complex-state-in-react-apps-with-mobx'
+				 },
+				 {
+				 	name: 'React: Flux Architecture (ES6)',
+				 	url: 'https://egghead.io/courses/react-flux-architecture-es6'
+				 },
+				 {
+				 	name: 'React Native Fundamentals',
+				 	url: 'https://egghead.io/courses/react-native-fundamentals'
+				 },
+				 {
+				 	name: 'Build Your First React.js App',
+				 	url: 'https://egghead.io/courses/build-your-first-react-js-application'
+				 },
+				 {
+				 	name: 'React Testing Cookbook',
+				 	url: 'https://egghead.io/courses/react-testing-cookbook'
+				 },
+				 {
+				 	name: 'Getting Started with React Router',
+				 	url: 'https://egghead.io/courses/getting-started-with-react-router'
+				 },
+				 {
+				 	name: 'Building React Applications with Idiomatic Redux',
+				 	url: 'https://egghead.io/courses/building-react-applications-with-idiomatic-redux'
+				 }
+			]
+		},
+		 {
+		 	name: 'Angular 2',
+		 	courses: [
+		 		{
+		 			name: 'Understand Angular 2 Directives in Depth',
+		 			url: 'https://egghead.io/courses/understand-angular-2-directives-in-depth'
+		 		},
+		 		{
+		 			name: 'Create Native Mobile Apps with NativeScript for Angular',
+		 			url: 'https://egghead.io/courses/create-native-mobile-apps-with-nativescript-for-angular'
+		 		},
+		 		{
+		 			name: 'Animate Angular 2 Web Applications',
+		 			url: 'https://egghead.io/courses/animate-angular-2-web-applications'
+		 		},
+		 		{
+		 			name: 'Learn the Basics of Angular 2 Forms',
+		 			url: 'https://egghead.io/courses/intro-to-angular-2-forms'
+		 		},
+		 		{
+		 			name: 'Manage UI State with the Angular 2 Router',
+		 			url: 'https://egghead.io/courses/intro-to-angular-2-router'
+		 		},
+		 		{
+		 			name: 'Build an Angular 2 Instant Search Component',
+		 			url: 'https://egghead.io/courses/build-an-angular-2-instant-search-component'
+		 		},
+		 		{
+		 			name: 'Angular 2 Dependency Injection (DI) Explained',
+		 			url: 'https://egghead.io/courses/angular-2-dependency-injection-di-explained'
+		 		},
+		 		{
+		 			name: 'Build Redux Style Applications with Angular2, RxJS, and ngrx/store',
+		 			url: 'https://egghead.io/courses/building-a-time-machine-with-angular-2-and-rxjs'
+		 		},
+		 		{
+		 			name: 'Building Angular 2 Components',
+		 			url: 'https://egghead.io/courses/building-angular-2-components'
+		 		},
+		 		{
+		 			name: 'Get Started with Angular 2',
+		 			url: 'https://egghead.io/courses/angular-2-fundamentals'
+		 		}
+		 	]
+		 },
+		 {
+		 	name: 'Angular 1.x',
+		 	courses: [
+		 		{
+		 			name: 'Build Angular 1.x Apps with Redux',
+		 			url: 'https://egghead.io/courses/build-angular-1-x-apps-with-redux'
+		 		},
+		 		{
+		 			name: 'Ionic Quickstart for Windows and Mac',
+		 			url: 'https://egghead.io/courses/ionic-quickstart-for-windows'
+		 		},
+		 		{
+		 			name: 'Learn Protractor Testing for AngularJS',
+		 			url: 'https://egghead.io/courses/learn-protractor-testing-for-angularjs'
+		 		},
+		 		{
+		 			name: 'Angular Automation with Gulp',
+		 			url: 'https://egghead.io/courses/angular-automation-with-gulp'
+		 		},
+		 		{
+		 			name: 'AngularJS Data Modeling',
+		 			url: 'https://egghead.io/courses/angularjs-data-modeling'
+		 		},
+		 		{
+		 			name: 'AngularJS Authentication with JWT',
+		 			url: 'https://egghead.io/courses/angularjs-authentication-with-jwt'
+		 		},
+		 		{
+		 			name: 'Angular and Webpack for Modular Applications',
+		 			url: 'https://egghead.io/courses/angular-and-webpack-for-modular-applications'
+		 		},
+		 		{
+		 			name: 'Introduction to Angular Material',
+		 			url: 'https://egghead.io/courses/angular-material-introduction'
+		 		},
+		 		{
+		 			name: 'AngularJS Application Architecture',
+		 			url: 'https://egghead.io/courses/angularjs-application-architecture'
+		 		},
+		 		{
+		 			name: 'AngularJS Fundamentals',
+		 			url: 'https://egghead.io/courses/angularjs-app-from-scratch-getting-started'
+		 		},
+		 		{
+		 			name: 'Using Angular 2 Patterns in Angular 1.x Apps',
+		 			url: 'https://egghead.io/courses/using-angular-2-patterns-in-angular-1-x-apps'
+		 		},
+		 		{
+		 			name: 'Building an Angular 1.x Ionic Application',
+		 			url: 'https://egghead.io/courses/building-an-angular-1-x-ionic-application'
+		 		}
+		 	]
+		 },
+		 {
+		 	name: 'RxJS',
+		 	courses: [
+		 		{
+		 			name: 'Cycle.js Fundamentals',
+		 			url: 'https://egghead.io/courses/cycle-js-fundamentals'
+		 		},
+		 		{
+		 			name: 'Use Higher Order Observables in RxJS Effectively',
+		 			url: 'https://egghead.io/courses/use-higher-order-observables-in-rxjs-effectively'
+		 		},
+		 		{
+		 			name: 'RxJS Subjects and Multicasting Operators',
+		 			url: 'https://egghead.io/courses/rxjs-subjects-and-multicasting-operators'
+		 		},
+		 		{
+		 			name: 'RxJS Beyond the Basics: Operators in Depth',
+		 			url: 'https://egghead.io/courses/rxjs-beyond-the-basics-operators-in-depth'
+		 		},
+		 		{
+		 			name: 'RxJS Beyond the Basics: Creating Observables from scratch',
+		 			url: 'https://egghead.io/courses/rxjs-beyond-the-basics-creating-observables-from-scratch'
+		 		},
+		 		{
+		 			name: 'Step-by-Step Async JavaScript with RxJS',
+		 			url: 'https://egghead.io/courses/step-by-step-async-javascript-with-rxjs'
+		 		},
+		 		{
+		 			name: 'Introduction to Reactive Programming',
+		 			url: 'https://egghead.io/courses/introduction-to-reactive-programming'
+		 		}
+		 	]
+		 },
+		 {
+		 	name: 'JavaScript',
+		 	courses: [
+		 		{
+		 			name: 'Natural Language Processing in JavaScript with Natural',
+		 			url: 'https://egghead.io/courses/natural-language-processing-in-javascript-with-natural'
+		 		},
+		 		{
+		 			name: 'Develop Web Apps with Vue.js',
+		 			url: 'https://egghead.io/courses/develop-web-apps-with-vue-js'
+		 		},
+		 		{
+		 			name: 'Build a GraphQL Server',
+		 			url: 'https://egghead.io/courses/build-a-graphql-server'
+		 		},
+		 		{
+		 			name: 'Build Complex 3D models with WebGL',
+	 			url: 'https://egghead.io/courses/build-complex-3d-models-with-webgl'
+	 		},
+	 		{
+	 			name: 'Professor Frisby Introduces Composable Functional JavaScript',
+	 			url: 'https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript'
+	 		},
+	 		{
+	 			name: 'Create 3D Graphics in JavaScript Using WebGL',
+	 			url: 'https://egghead.io/courses/create-3d-graphics-in-javascript-using-webgl'
+	 		},
+	 		{
+	 			name: 'Creating Custom Web Elements with Polymer',
+	 			url: 'https://egghead.io/courses/creating-custom-web-elements-with-polymer'
+	 		},
+	 		{
+	 			name: 'Learn how to use Immutable.js',
+	 			url: 'https://egghead.io/courses/learn-how-to-use-immutable-js'
+	 		},
+	 		{
+	 			name: 'How to Write an Open Source JavaScript Library',
+	 			url: 'https://egghead.io/courses/how-to-write-an-open-source-javascript-library'
+	 		},
+	 		{
+	 			name: 'How to Contribute to an Open Source Project on GitHub',
+	 			url: 'https://egghead.io/courses/how-to-contribute-to-an-open-source-project-on-github'
+	 		},
+	 		{
+	 			name: 'Understand JavaScript Arrays',
+	 			url: 'https://egghead.io/courses/javascript-arrays-in-depth'
+	 		},
+	 		{
+	 			name: 'Advanced Logging with the JavaScript Console',
+	 			url: 'https://egghead.io/courses/js-console-for-power-users'
+	 		},
+	 		{
+	 			name: 'Learn HTML5 Graphics and Animation',
+	 			url: 'https://egghead.io/courses/learn-html5-graphics-and-animation'
+	 		},
+	 		{
+	 			name: 'Regex in Javascript',
+	 			url: 'https://egghead.io/courses/regex-in-javascript'
+	 		},
+	 		{
+	 			name: 'Asynchronous Programming: The End of The Loop',
+	 			url: 'https://egghead.io/courses/mastering-asynchronous-programming-the-end-of-the-loop'
+	 		},
+	 		{
+	 			name: 'Reduce Data with Javascript Array#reduce',
+	 			url: 'https://egghead.io/courses/reduce-data-with-javascript'
+	 		},
+	 		{
+	 			name: 'GIF Loop Coder for Creating Animation',
+	 			url: 'https://egghead.io/courses/gif-loop-coder-for-creating-animation'
+	 		},
+	 		{
+	 			name: 'Using Webpack for Production JavaScript Applications',
+	 			url: 'https://egghead.io/courses/using-webpack-for-production-javascript-applications'
+	 		},
+	 		{
+	 			name: 'Learn ES6 (ECMAScript 2015)',
+	 			url: 'https://egghead.io/courses/learn-es6-ecmascript-2015'
+	 		}
+	 	]
+	 },
+	 {
+	 	name: 'NodeJs',
+	 	courses: [
+	 		{
+	 			name: 'Introduction to Node Servers with Hapi.js',
+	 			url:  'https://egghead.io/courses/introduction-to-node-servers-with-hapi-js'
+	 		},
+	 		{
+	 			name: 'Introduction to Node: The Fundamentals',
+	 			url: 'https://egghead.io/courses/introduction-to-node-the-fundamentals'
+	 		},
+	 		{
+	 			name: 'Getting Started with Express.js',
+	 			url: 'https://egghead.io/courses/getting-started-with-express-js'
+	 		},
+	 		{
+	 			name: 'Build Node.js APIs with OpenAPI Spec (Swagger)',
+	 			url: 'https://egghead.io/courses/build-node-js-apis-with-openapi-spec-swagger'
+	 		}
+	 	]
+	 },
+	 {
+	 	name: 'CSS',
+	 	courses: [
+	 		{
+	 			name: 'Flexbox Fundamentals',
+	 			url: 'https://egghead.io/courses/flexbox-fundamentals'
+	 		},
+	 		{
+	 			name: 'Learn Advanced CSS Layout Techniques',
+	 			url: 'https://egghead.io/courses/learn-advanced-css-layout-techniques'
+	 		}
+	 	]	
+	 },
+	 {
+	 	name: 'ELM',
+	 	courses: [
+	 		{
+	 			name: 'Start Using Elm to Build Web Applications',
+	 			url: 'https://egghead.io/courses/start-using-elm-to-build-web-applications'
+	 		}
+	 	]
+	 },
+	 {
+	 	name: 'D3',
+	 	courses: [
+	 		{
+	 			name: 'Build Interactive JavaScript Charts with D3 v4',
+	 			url: 'https://egghead.io/courses/build-interactive-javascript-charts-with-d3-v4' 
+	 		},
+	 		{
+	 			name: 'Use D3 (v3) to Build Interactive Charts with JavaScript',
+	 			url: 'https://egghead.io/courses/introduction-to-d3'
+	 		}
+	 	]
+	 },
+	 {
+	 	name: 'otherTechonolgies',
+	 	courses: [
+	 		{
+	 			name: 'Deploy Web Apps with Zeit Now',
+	 			url: 'https://egghead.io/courses/deploy-web-apps-with-zeit-now'
+	 		},
+	 		{
+	 			name: 'Record Badass Screencasts for egghead.io',
+	 			url: 'https://egghead.io/courses/record-badass-screencasts-for-egghead-io'
+	 		},
+	 		{
+	 			name: 'Debug HTTP with Chrome DevTools Network Panel',
+	 			url: 'https://egghead.io/courses/chrome-devtools-network-panel'
+	 		},
+	 		{
+	 			name: 'Debug JavaScript in Chrome with DevTool Sources',
+	 			url: 'https://egghead.io/courses/chrome-devtools-sources-panel'
+	 		},
+	 		{
+	 			name: 'Debug the DOM in Chrome with the Devtools Elements Panel',
+	 			url: 'https://egghead.io/courses/using-chrome-developer-tools-elements'
+	 		},
+	 		{
+	 			name: 'Start Building Accessible Web Applications Today',
+	 			url: 'https://egghead.io/courses/start-building-accessible-web-applications-today'
+	 		},
+	 		{
+	 			name: 'Practical Git for Everyday Professional Use',
+	 			url: 'https://egghead.io/courses/practical-git-for-everyday-professional-use'
+	 		},
+	 		{
+	 			name: 'Learn to Use VIM',
+	 			url: 'https://egghead.io/courses/learn-to-use-vim'
+	 		}
+	 	]
+	 },
+	 {
+	 	name: 'TypeScript',
+	 	courses: [
+	 		{
+	 			name: 'Use Types Effectively in TypeScript',
+	 			url: 'https://egghead.io/courses/use-types-effectively-in-typescript'
+	 		},
+	 		{
+	 			name: 'Up and Running with TypeScript',
+	 			url: 'https://egghead.io/courses/up-and-running-with-typescript'
+	 		}
+	 	]
+	 }
+];
+let command = 'egghead-downloader -e ionutvalentin66@gmail.com -p1234567 -c';
+
+let path = require('path');
+
+function execution(url, techName, courseName) {
+	const exec = require('child_process').execSync;
+	
+	let ourPath = path.win32.join(__dirname, techName, courseName);
+
+	const child = exec(`egghead-downloader -e ionutvalentin66@gmail.com -p omega1992 -c ${url} "${ourPath}"`, (error, stdout, stderr) => {
+		console.log(stdout);
+  		console.log(stderr);
+
+  		if (error) {
+    		throw error;
+  		}
+	});
+}
+
+technologies.forEach(function (technology) {
+	const fs = require('fs');
+
+	let techName = technology.name.replace(/\s/g, '-');
+
+	if (!fs.existsSync(techName)){
+	    fs.mkdirSync(techName);
+	}
+
+	technology.courses.forEach(function (course) {
+		execution(course.url, technology.name.replace(/\s/g, '-'), course.name.replace(/\s/g, '-'));
+	});
+});
